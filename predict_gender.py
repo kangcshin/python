b195899a2e28611a763cbcd5e2685d4070000d9f
Kang Shin <kshin33@gmail.com>
2018-03-07 15:41:46 -0500

    Update

--- predict_gender.py
+++ predict_gender.py
@@ -1,4 +1,9 @@
 from sklearn import tree
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import Perceptron
+import numpy as np
 
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [190, 90, 47], [175, 64, 39],
@@ -7,8 +12,33 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
-classifier1 = tree.DecisionTreeClassifier()
-classifier1 = classifier1.fit(X,Y)
-prediction = classifier1.predict([[190,70,43]])
+classifier_tree = tree.DecisionTreeClassifier()
+classifier_svm = SVC()
+classifier_KNN = KNeighborsClassifier()
+classifier_perceptron = Perceptron()
 
-print(prediction)
+classifier_tree.fit(X,Y)
+classifier_svm.fit(X,Y)
+classifier_KNN.fit(X,Y)
+classifier_perceptron.fit(X,Y)
+
+prediction_tree = classifier_tree.predict(X)
+accuracy_tree = accuracy_score(Y, prediction_tree) * 100
+print('Accuracy for DecisionTree: {}'.format(accuracy_tree))
+
+prediction_svm = classifier_svm.predict(X)
+accuracy_svm = accuracy_score(Y, prediction_svm) * 100
+print('Accuracy for SVM: {}'.format(accuracy_svm))
+
+prediction_KNN = classifier_KNN.predict(X)
+accuracy_KNN = accuracy_score(Y, prediction_KNN) * 100
+print('Accuracy for KNN: {}'.format(accuracy_KNN))
+
+prediction_perceptron = classifier_perceptron.predict(X)
+accuracy_perceptron = accuracy_score(Y, prediction_perceptron) * 100
+print('Accuracy for Perceptron: {}\n'.format(accuracy_perceptron))
+
+#Selecting classifier with highest accuracy
+index = np.argmax([accuracy_svm, accuracy_KNN, accuracy_perceptron])
+names = {0:'SVM', 1:'KNN', 2:'Perceptron'}
+print('The most accurate classifier is {}!\n'. format(names[index]))
